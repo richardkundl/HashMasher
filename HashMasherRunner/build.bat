bf252879d0f0369e7383517bf91370384f164711
DetroitPro <eric@polerecky.com>
2012-01-17 01:08:47 -0500

    path fix

--- HashMasherRunner/build.bat
+++ HashMasherRunner/build.bat
@@ -1,1 +1,1 @@
-﻿"C:\Program Files (x86)\Jenkins\jobs\HashMash-Service\workspace\Assets\Phantom\Phantom\phantom.exe" -f "C:\Program Files (x86)\Jenkins\jobs\HashMasher\workspace\HashMasherRunner\build.boo"
+﻿"C:\Program Files (x86)\Jenkins\jobs\HashMasher\workspace\Assets\Phantom\Phantom\phantom.exe" -f "C:\Program Files (x86)\Jenkins\jobs\HashMasher\workspace\HashMasherRunner\build.boo"
