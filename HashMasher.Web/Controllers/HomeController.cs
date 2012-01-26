459ef04a66486529b001f11df25e9538e1617d23
DetroitPro <eric@polerecky.com>
2012-01-26 01:33:28 -0500

    more theme. sidebar.

--- HashMasher.Web/Controllers/HomeController.cs
+++ HashMasher.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using HashMasher.Model;
 using ProMongoRepository;
@@ -10,22 +11,39 @@ namespace HashMasher.Web.Controllers
     {
         protected readonly ILog _logger = LogManager.GetLogger("HomeController");
         private readonly IMongoRepository<ProcessedLink> _repository;
+        private readonly IApplicationConfiguration _configuration;
 
-        public HomeController(IMongoRepository<ProcessedLink> repository)
+        public HomeController(IMongoRepository<ProcessedLink> repository, IApplicationConfiguration configuration)
         {
             _repository = repository;
+            _configuration = configuration;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string id)
         {
-            
 
-            var vm = _repository
-                .Linq()
-                .OrderByDescending(x => x.NumberOfTweets)
-                .OrderByDescending(x => x.Created)
-                .ToList();
+            ViewBag.Tag = "ALL";
+            ViewBag.Tracked = _configuration.HashTags.Split(',').ToArray();
+            List<ProcessedLink> vm = new List<ProcessedLink>();
+            if(string.IsNullOrEmpty(id))
+            {
+                vm = _repository
+                    .Linq()
+                    .OrderByDescending(x => x.NumberOfTweets)
+                    .OrderByDescending(x => x.Created)
+                    .ToList();
+            } else
+            {
 
+                ViewBag.Tag = id.ToUpper();
+                var searchKey = id.Replace("#", "").Trim();
+                vm = _repository
+                    .Linq()
+                    .Where(x => x.HashTag == searchKey)
+                    .OrderByDescending(x => x.NumberOfTweets)
+                    .OrderByDescending(x => x.Created)
+                    .ToList();
+            }
             _logger.Info("items:" + vm.Count());
             return View(vm);
         }
